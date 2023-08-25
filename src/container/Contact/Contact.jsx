58c20c326e924be0331820d528c7997a683f6aea
Caleb Tseng <calebtseng07@gmail.com>
2023-08-25 05:18:51 -0700

    Create contact page

---
+++ src/container/Contact/Contact.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className='estate__contact contact__bg section__padding'>
+            <div className="estate__contact-content">
+                <div className="estate__contact-content_text">
+                    <p className='p__futura'>What are you waiting for?</p>
+                    <p className='p__futura text__underline'>Start detecting today.</p>
+                </div>
+
+                <div className="estate__contact-content_info">
+                    <button className='button'>Create an account</button>
+                    <p className='p_basier'>Do you have a question?</p>
+                    <p className='p_basier'>Don't hesitate!</p>
+                    <p className='p_basier'>hello@usesafewatch.com</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
