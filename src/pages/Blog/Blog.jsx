e2b54919fe4fdb80b98be4e77bcca5e8387ab88d
Caleb Tseng <calebtseng07@gmail.com>
2023-09-02 05:44:18 -0700

    Add blog page

---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,13 @@
+import MainLayout from '../../layout/MainLayout';
+
+import './Blog.css';
+
+const Blog = () => {
+    return (
+        <MainLayout>
+            <div>Blog Page</div>
+        </MainLayout>
+    );
+};
+
+export default Blog;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Detectors from "./Detectors/Detectors";
 import Heatmaps from "./Heatmaps/Heatmaps";
 import Integrations from "./Integrations/Integrations";
+import Blog from "./Blog/Blog";
 
 export {
     Detectors,
     Heatmaps,
     Integrations,
+    Blog,
 };
