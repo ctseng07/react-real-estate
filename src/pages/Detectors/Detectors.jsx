a2e50d1a647a28edf760d9e9ccc8dca4d577c6f6
Caleb Tseng <calebtseng07@gmail.com>
2023-09-03 10:47:25 -0700

    Start styling detectors page

--- src/pages/Detectors/Detectors.jsx
+++ src/pages/Detectors/Detectors.jsx
@@ -1,11 +1,23 @@
 import MainLayout from '../../layout/MainLayout';
+import DetectorsBG from '../../assets/noiseabstract.jpg';
 
 import './Detectors.css';
 
 const Detectors = () => {
     return (
         <MainLayout>
-            <div>Detectors Page</div>
+            <div className="estate__detectors">
+                <div className="estate__detectors-container">
+                    <div className="estate__detectors-img">
+                        <img src={DetectorsBG} alt="detect__img" />
+                        <h1 className='p__cerebri'>Cut through the noise and get alerted quicker</h1>
+                        <div className="estate__detectors-heading">
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </MainLayout>
     );
 };
