95f4780401a8cbba6da9eaf51acf08727d9e50f2
Caleb Tseng <calebtseng07@gmail.com>
2023-09-04 22:35:03 -0700

    Add styling to detectors page

--- src/pages/Detectors/Detectors.jsx
+++ src/pages/Detectors/Detectors.jsx
@@ -6,16 +6,23 @@ import './Detectors.css';
 const Detectors = () => {
     return (
         <MainLayout>
-            <div className="estate__detectors">
-                <div className="estate__detectors-container">
-                    <div className="estate__detectors-img">
-                        <img src={DetectorsBG} alt="detect__img" />
-                        <h1 className='p__cerebri'>Cut through the noise and get alerted quicker</h1>
-                        <div className="estate__detectors-heading">
-                        </div>
-                    </div>
+            <div className="estate__detectors detector__bg">
+                <div className="estate__detectors-heading flex__center">
+                    <h1 className='p__cerebri'>Cut through the noise and get alerted quicker</h1>
                 </div>
+            </div>
 
+            <div className="estate__detectors-content">
+                <h1 className='p__cerebri'>Safewatch will give super powers to your security cameras!</h1>
+                <p className='p__cerebri '>Safewatch uses state-of-the art artificial intelligence to detect objects in your security feed. You can select the objects you want to be alerted on, and when they are detected you will receive a text notification to your phone.</p>
+                <p className='p__cerebri'>On top of this you get free 14 days of footage storage so you can go back to watch your camera feed when something happened, and you can download a copy.</p>
+            </div>
+
+            <div className="estate__detectors-features">
+
+            </div>
+
+            <div className="estate__detectors-video">
 
             </div>
         </MainLayout>
