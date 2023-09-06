4d1036e8ac2d172598b28a660381deb86f267cad
Caleb Tseng <calebtseng07@gmail.com>
2023-09-05 21:58:30 -0700

    Create constants folder to put images in

---
+++ src/constants/images.js
@@ -0,0 +1,17 @@
+import science from '../assets/icons/biohazard-svgrepo-com.svg';
+import computer from '../assets/icons/computer-svgrepo-com.svg';
+import rocket from '../assets/icons/rocket-svgrepo-com.svg';
+// import present from '../assets/icons/present-svgrepo-com.svg';
+import contact from '../assets/icons/add-contact-svgrepo-com.svg';
+import update from '../assets/icons/status-scheduled-svgrepo-com.svg';
+
+
+export default {
+    science,
+    computer,
+    rocket,
+    // present,
+    contact,
+    update,
+};
+
