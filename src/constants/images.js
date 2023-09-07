914c9ec1e5bfd204c0d93c5a60a0b67b6be1d659
Caleb Tseng <calebtseng07@gmail.com>
2023-09-06 21:58:14 -0700

    Add more assets to constants

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
 import science from '../assets/icons/biohazard-svgrepo-com.svg';
 import computer from '../assets/icons/computer-svgrepo-com.svg';
 import rocket from '../assets/icons/rocket-svgrepo-com.svg';
-// import present from '../assets/icons/present-svgrepo-com.svg';
+import present from '../assets/icons/present-svgrepo-com.svg';
 import contact from '../assets/icons/add-contact-svgrepo-com.svg';
 import update from '../assets/icons/status-scheduled-svgrepo-com.svg';
 
@@ -10,7 +10,7 @@ export default {
     science,
     computer,
     rocket,
-    // present,
+    present,
     contact,
     update,
 };
