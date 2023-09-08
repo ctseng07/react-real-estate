f98136dfed49b8fafe3d960c1ce17c65bf92c39b
Caleb Tseng <calebtseng07@gmail.com>
2023-09-08 13:25:56 -0700

    Add demo vid component

---
+++ src/components/DemoVid/DemoVid.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './DemoVid.css'
+
+const DemoVid = () => {
+    return (
+        <div className='DemoVid demo__bg'>
+            <button className='flex__center'>Play</button>
+        </div>
+    )
+}
+
+export default DemoVid
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from './Navbar/Navbar';
+import DemoVid from './DemoVid/DemoVid';
 import Footer from './Footer/Footer';
 
 export {
     Navbar,
+    DemoVid,
     Footer
 };
