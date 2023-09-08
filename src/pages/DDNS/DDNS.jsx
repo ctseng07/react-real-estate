8a4e424e2aba15048052a27302c5694ce3779166
Caleb Tseng <calebtseng07@gmail.com>
2023-09-08 12:35:02 -0700

    Add DDNS file

---
+++ src/pages/DDNS/DDNS.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './DDNS.css';
+
+const DDNS = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default DDNS
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Heatmaps from "./Heatmaps/Heatmaps";
 import Integrations from "./Integrations/Integrations";
 import Blog from "./Blog/Blog";
 import LoginPage from "./LoginPage/LoginPage";
+import DDNS from "./DDNS/DDNS";
 
 export {
     Detectors,
     Heatmaps,
     Integrations,
     Blog,
-    LoginPage
+    LoginPage,
+    DDNS
 };
