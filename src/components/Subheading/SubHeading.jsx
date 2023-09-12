ddd16f75bfa40da9f4cddb0fa446be69a277b5a5
Caleb Tseng <calebtseng07@gmail.com>
2023-09-12 04:09:17 -0700

    Create SUbHEading folder

---
+++ src/components/Subheading/SubHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SubHeading = ({ title }) => (
+    <div className="SubHeading">
+        <p className='SubHeading-title'>{title}</p>
+    </div>
+);
+
+export default SubHeading;
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Navbar from './Navbar/Navbar';
+import SubHeading from './Subheading/SubHeading';
 import DemoVid from './DemoVid/DemoVid';
 import Footer from './Footer/Footer';
 
 export {
     Navbar,
+    SubHeading,
     DemoVid,
     Footer
 };
