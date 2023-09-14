d619b7fd2e1764a9111405ecccd06f70e5554b01
Caleb Tseng <calebtseng07@gmail.com>
2023-09-14 04:05:16 -0700

    Create blog page

---
+++ src/pages/BlogPage/BlogPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainLayout from '../../layout/MainLayout';
+
+import './BlogPage.css'
+
+const BlogPage = () => {
+    return (
+        <MainLayout>
+            <div>Blog Page</div>
+        </MainLayout>
+    )
+}
+
+export default BlogPage
