8266122cec26fb71afad95e5339038779b939e52
Caleb Tseng <calebtseng07@gmail.com>
2023-09-19 19:52:03 -0700

    Start styling and designing header and content of DDNS page

--- src/pages/DDNS/DDNS.jsx
+++ src/pages/DDNS/DDNS.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SecondLayout from '../../layout/SecondLayout';
 
-import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { FiFacebook, FiTwitter, FiLinkedin } from 'react-icons/fi';
 import Fiber from '../../assets/fiber.jpeg';
 import Graph from '../../assets/graph.png';
+
+import Chronometer from '../../assets/chronometer-stopwatch-svgrepo-com.svg';
 
 import './DDNS.css';
 
@@ -19,18 +21,39 @@ const DDNS = () => {
                         <p className='p__cerebri'>"Dynamic DNS is a method of automatically updating a name server in the Domain Name Server, often in real time, with the active DDNS configuration of its configured hostnames, addresses or other information."
                             -Wikipedia</p>
 
-                        <div className="ddns__socialIcons">
+                        <div className="ddns__social">
                             <FiFacebook />
                             <FiTwitter />
-                            <FiInstagram />
+                            <FiLinkedin />
                         </div>
                     </div>
-
                     <div className="ddns__header-img">
-                        <img src={Graph} alt="graph__img" />
                         <img src={Fiber} alt="fiber__img" />
+                        <img src={Graph} alt="graph__img" />
                     </div>
                 </div>
+            </div>
+
+            <div className="ddns__info-container">
+                <div className="ddns__info-wrapper">
+                    <div className="ddns__info-left">
+                        <div className="ddns__container-icon flex__center">
+                            <img src={Chronometer} alt="chronometer-icon" />
+                        </div>
+                        <h1 className='p__neuzeit'>Easy setup and management</h1>
+                        <p className='p__cerebri'>Getting started is super easy</p>
+                        <div className="text-dash"></div>
+                    </div>
+
+                    <div className="ddns__info-right">
+                        <h1 className='p__neuzeit'>4 Easy steps</h1>
+                        <p className='p__cerebri'>1. Login with your Safewatch account.</p>
+                        <p className='p__cerebri'>2. Enter a hostname you want to use.</p>
+                        <p className='p__cerebri'>3. Leave the ip-updater running.</p>
+                        <p className='p__cerebri'>4. Voilà!</p>
+                    </div>
+                </div>
+
             </div>
         </SecondLayout>
     )
