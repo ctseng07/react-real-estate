a4a50fc0be4c14a6924ff2def06916393dec40dc
Caleb Tseng <calebtseng07@gmail.com>
2023-09-21 17:33:08 -0700

    Styling more of the ddns page started the download section

--- src/pages/DDNS/DDNS.jsx
+++ src/pages/DDNS/DDNS.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import SecondLayout from '../../layout/SecondLayout';
 
 import { FiFacebook, FiTwitter, FiLinkedin } from 'react-icons/fi';
 import Fiber from '../../assets/fiber.jpeg';
 import Graph from '../../assets/graph.png';
+
+import Files from '../../assets/files.jpeg';
 
 import Chronometer from '../../assets/chronometer-stopwatch-svgrepo-com.svg';
 
@@ -12,7 +15,7 @@ import './DDNS.css';
 const DDNS = () => {
     return (
         <SecondLayout>
-            <div className="ddns ddns__bg">
+            <div className="ddns">
                 <div className="ddns__content ddns__bg">
                     <div className="ddns__header-info">
                         <h1 className='p__neuzeit'>Free Dynamic DNS</h1>
@@ -40,22 +43,37 @@ const DDNS = () => {
                         <div className="ddns__container-icon flex__center">
                             <img src={Chronometer} alt="chronometer-icon" />
                         </div>
-                        <h1 className='p__neuzeit'>Easy setup and management</h1>
+                        <h2 className='p__neuzeit'>Easy setup and management</h2>
                         <p className='p__cerebri'>Getting started is super easy</p>
                         <div className="text-dash"></div>
                     </div>
 
                     <div className="ddns__info-right">
-                        <h1 className='p__neuzeit'>4 Easy steps</h1>
+                        <h2 className='p__neuzeit'>4 Easy steps</h2>
                         <p className='p__cerebri'>1. Login with your Safewatch account.</p>
                         <p className='p__cerebri'>2. Enter a hostname you want to use.</p>
                         <p className='p__cerebri'>3. Leave the ip-updater running.</p>
                         <p className='p__cerebri'>4. Voilà!</p>
                     </div>
                 </div>
-
             </div>
-        </SecondLayout>
+
+            <div className="ddns__download">
+                <img src={Files} alt="files_img" />
+                <div className="ddns__download-content flex__center">
+                    <div className="download-dash"></div>
+                    <h2 className='p__neuzeit'>Get started by downloading the client.</h2>
+                    <p className='p__cerebri'>You need an account that can be obtained at: </p>
+                    <p className='p__cerebri'>
+                        <Link to='/LoginPage'>https://dashboard.usesafewatch.com/</Link></p>
+
+                    {/* <div className="ddns__download-buttons"> */}
+                    <button className='button text__uppercase'>Apple <br />macos</button>
+                    <button className='button text__uppercase'>windows <br />(coming soon)</button>
+                    {/* </div> */}
+                </div >
+            </div>
+        </SecondLayout >
     )
 }
 
